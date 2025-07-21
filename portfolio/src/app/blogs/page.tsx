f64f647e5e8ab1fb533595d7be7911df4b4bb021
A1iHassan <ali012wkout@gmail.com>
2025-07-21 23:46:52 +0300

    deugging

--- portfolio/src/app/blogs/page.tsx
+++ portfolio/src/app/blogs/page.tsx
@@ -14,24 +14,28 @@ const Blogs = async () => {
     "
     >
       <div className="mt-20 flex flex-col items-center sm:flex-row flex-wrap gap-10">
-        {data.map((blog) => (
-          <Link
-            key={blog.id}
-            href={`blogs/${blog.id}`}
-            className="w-56 h-56 relative shadow-lg shadow-blue-50 rounded-lg flex items-end p-3 cursor-pointer overflow-hidden group
+        {data
+          ? data.map((blog) => (
+              <Link
+                key={blog.id}
+                href={`blogs/${blog.id}`}
+                className="w-56 h-56 relative shadow-lg shadow-blue-50 rounded-lg flex items-end p-3 cursor-pointer overflow-hidden group
           sm:w-72 sm:h-86
           "
-          >
-            <Image
-              src={blog.cover}
-              alt="Background"
-              fill
-              className="object-cover rounded-lg z-0"
-            />
-            <div className="absolute inset-0 bg-gradient-to-b from-60% from-transparent to-blue-200 z-10" />
-            <span className="z-10 text-blue-900 sm:text-2xl">{blog.title}</span>
-          </Link>
-        ))}
+              >
+                <Image
+                  src={blog.cover}
+                  alt="Background"
+                  fill
+                  className="object-cover rounded-lg z-0"
+                />
+                <div className="absolute inset-0 bg-gradient-to-b from-60% from-transparent to-blue-200 z-10" />
+                <span className="z-10 text-blue-900 sm:text-2xl">
+                  {blog.title}
+                </span>
+              </Link>
+            ))
+          : "no data was found"}
       </div>
     </div>
   );
